56a3d4ebd32de3137bd70b0a136dace490ca74b5
volante1013 <u.swb01140@gmail.com>
2018-04-27 01:35:14 +0900

    Test Kudu deploy

--- KancolleTweetWatcher/NotifyFunc.cs
+++ KancolleTweetWatcher/NotifyFunc.cs
@@ -49,7 +49,6 @@ namespace KancolleTweetWatcher
 			};
 
 			ai.TrackTrace(client.PushLink(request).ToJson(), Microsoft.ApplicationInsights.DataContracts.SeverityLevel.Verbose);
-			//log.Info(client.PushLink(request).ToJson());
 			//return req.CreateResponse(HttpStatusCode.OK, client.PushLink(request));
 		}
 	}
